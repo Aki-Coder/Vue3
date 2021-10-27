c8e2b5392ab56b2c8d31ee1b76aaaf0b93f5b3b0
Andreja Simić <andrejas@yuspin.rs>
2021-10-27 15:49:26 +0200

    computed properties

--- main.js
+++ main.js
@@ -5,21 +5,24 @@ const app = Vue.createApp({
     data(){
         return{
             product:'Socks',
+            brand:'Vue Mastery',
             description:'Soft socks',
-            image:'./assets/images/socks_green.jpg',
+            //image:'./assets/images/socks_green.jpg',
+            selectedVariant:0,
             url:'https://codepen.io/VueMastery/project/editor/AqemLM',
-            inStock:false,
+            //inStock:true,
             inventory:10,
             onSale:true,
             details:['50% cotton', '30% wool', '20% polyester'],
             variants:[
-                {id:2234, color:'green', image:'./assets/images/socks_green.jpg'},
-                {id:2235, color:'blue', image:'./assets/images/socks_blue.jpg'}
+                {id:2234, color:'green', image:'./assets/images/socks_green.jpg',quantity:50},
+                {id:2235, color:'blue', image:'./assets/images/socks_blue.jpg', quantity:0},
             ],
             sizes:['s','m','l','xl'],
             cart:0,
             buttonAdd:'Add to Cart',
-            buttonCancel:'Out of Cart'
+            buttonCancel:'Out of Cart',
+            onSale:true,
         }
     },
     methods:{
@@ -33,6 +36,33 @@ const app = Vue.createApp({
         },
         updateImage(variantImage){
             this.image = variantImage;
+        },
+        updateVariant(index){
+            this.selectedVariant = index;
+        },
+    },
+    /* Calculated value for us (Computed property)
+       Benefir of computed properties is that they cash the value.
+       Stores it away and only updates when it needs to */
+    computed:{
+        title(){
+            return this.brand + ' ' + this.product; 
+        },
+        image(){
+            //variants is array
+            //0 target first line in array, 1 target second line
+            //selectedVarinat is index
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock(){
+            //grab quantity from array
+            return this.variants[this.selectedVariant].quantity;
+        },
+        saleMessage(){
+            if(this.onSale){
+                return this.brand + ' ' + this.product + ' is on sale.'
+            }
+            return ''
         }
-    }
+    },
 }) 
